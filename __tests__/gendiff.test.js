455cc459d5c5f5c90c34101cfa438eeff996625e
Aleksandr Dargeev <dargeev@gmail.com>
2019-06-03 10:05:47 +0700

    remove absolute path testing causing travis' error

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,7 @@
 import { readFileSync } from 'fs';
 import gendiff from '../src';
 
-test('gendiff relative path', () => {
+test('gendiff', () => {
   const result = readFileSync('./__tests__/__fixtures__/result.txt', 'utf8');
   expect(gendiff('before.json', 'after.json', 'test3')).toBe(result);
-});
-
-test('gendiff absolute path', () => {
-  const result = readFileSync('./__tests__/__fixtures__/result.txt', 'utf8');
-  expect(gendiff('/home/alex/projects/gendiff/before.json', '/home/alex/projects/gendiff/after.json', 'test3')).toBe(result);
 });
